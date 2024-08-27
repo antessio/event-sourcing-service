dc42fa8ad6eb96d030e94f93efb3c5e47cb12957
antessio <antessio7@gmail.com>
2024-08-27 14:30:05 +0200

    cleanup

--- test-utils/src/test/java/utils/SystemUtils.java
+++ test-utils/src/test/java/utils/SystemUtils.java
@@ -10,18 +10,9 @@ public final class SystemUtils {
     }
 
     public static boolean isTestContainerEnabled() {
-        String testContainerEnabled = System.getenv("testContainerEnabled");
-        System.out.println("testContainerEnabled = " + testContainerEnabled);
-        return Optional.ofNullable(testContainerEnabled).map(Boolean::valueOf).orElse(false);
+        return Optional.ofNullable(System.getenv("testContainerEnabled")).map(Boolean::valueOf).orElse(false);
     }
     public static String getPostgresUrl(){
-        if (isTestContainerEnabled()){
-            return PostgresContainer.getUrl();
-        }else{
-            return "jdbc:postgresql://localhost:5432/antessio_event_sourcing";
-        }
-    }
-    public static String getPostgresUser(){
         if (isTestContainerEnabled()){
             return PostgresContainer.getUrl();
         }else{
